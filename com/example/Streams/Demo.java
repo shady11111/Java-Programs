0708e5d7c0194fa6f30e97d681202540fb1f123b
shady <speakingtreemind@gmail.com>
2023-12-02 21:23:33 +0530

    2/12/2023

--- com/example/Streams/Demo.java
+++ com/example/Streams/Demo.java
@@ -29,9 +29,9 @@ public class Demo {
 		hundredClub.forEach(person->System.out.println(person.name));
 		
 		System.out.println("Sorted By Name:");
-		List<Person> sortedByName=people.stream().
-				sorted(Comparator.comparing(person->person.name)).collect(Collectors.toList());
-		sortedByName.forEach(person->System.out.println(person.name));
+		List<Person> sortedByName=people.stream().sorted(Comparator.comparing(person->person.name)).
+										 		  collect(Collectors.toList());
+			sortedByName.forEach(person->System.out.println(person.name));
 		
 		System.out.println("Hundred Dollar Club Sorted By Name:");
 		List<Person> hundredClubSortedByName=people.stream().filter(person->person.billions>=100).
